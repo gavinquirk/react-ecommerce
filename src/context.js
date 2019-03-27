2da66eda4e4e7afbf37750fab3044457a28d87dc
Gavin Quirk <gavinquirk@gmail.com>
2019-03-26 17:21:23 -0700

    cart components

--- src/context.js
+++ src/context.js
@@ -9,9 +9,12 @@ class ProductProvider extends Component {
   state = {
     products: [],
     detailProduct: detailProduct,
-    cart: [],
+    cart: storeProducts,
     modalOpen: false,
-    modalProduct: detailProduct
+    modalProduct: detailProduct,
+    cartSubTotal: 0,
+    cartTax: 0,
+    cartTotal: 0
   };
 
   componentDidMount() {
@@ -72,6 +75,22 @@ class ProductProvider extends Component {
     });
   };
 
+  increment = id => {
+    console.log('this is the increment method');
+  };
+
+  decrement = id => {
+    console.log('this is the decrement method');
+  };
+
+  removeItem = id => {
+    console.log('item removed');
+  };
+
+  clearCart = () => {
+    console.log('cart cleared');
+  };
+
   render() {
     return (
       <ProductContext.Provider
@@ -80,7 +99,11 @@ class ProductProvider extends Component {
           handleDetail: this.handleDetail,
           addToCart: this.addToCart,
           openModal: this.openModal,
-          closeModal: this.closeModal
+          closeModal: this.closeModal,
+          increment: this.increment,
+          decrement: this.decrement,
+          removeItem: this.removeItem,
+          clearCart: this.clearCart
         }}
       >
         {this.props.children}
